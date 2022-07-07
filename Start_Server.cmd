b6434a9b53604789f780a73c866a69c256d8f4c9
AbrahamMoody <52460339+AbrahamMoody@users.noreply.github.com>
2022-07-08 01:01:04 +0200

    FULL-ESX-WITH-QUASAR

---
+++ Start_Server.cmd
@@ -0,0 +1,2 @@
+cd C:\fivelife2022
+C:\fivelife2022\FXServer.exe +set serverProfile "server2" +set txAdminPort "40125"
---
+++ resources/[inventory]/qs-shops/config/config_framework.lua
@@ -0,0 +1,5 @@
+Config.Framework = 'ESX' -- 'ESX' or 'QBCore'.
+
+-- Ignore this if you are using QBCore
+Config.getSharedObject = 'esx:getSharedObject'
+Config.setJob = 'esx:setJob'
