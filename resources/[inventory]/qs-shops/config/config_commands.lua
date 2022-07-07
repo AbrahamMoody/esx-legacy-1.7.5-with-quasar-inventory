662c43bea6ce910ca00aa5915b6d20ae4cbc71c7
AbrahamMoody <52460339+AbrahamMoody@users.noreply.github.com>
2022-07-08 01:28:47 +0200

    Delete resources/[inventory] directory

--- resources/[inventory]/qs-shops/config/config_commands.lua
+++
@@ -1,5 +0,0 @@
--- Command to open the menu
-
-RegisterCommand("customshop", function(source, args)
-    TriggerClientEvent('qs-shops:customshopmenu', source, true)
-end, true)
--- resources/[inventory]/qs-shops/config/config_framework.lua
+++
@@ -1,5 +0,0 @@
-Config.Framework = 'ESX' -- 'ESX' or 'QBCore'.
-
--- Ignore this if you are using QBCore
-Config.getSharedObject = 'esx:getSharedObject'
-Config.setJob = 'esx:setJob'
